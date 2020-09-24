c77f6a89ef7b522004e1ada93b9affc1db5bd727
neeraj <neerajmaran1199@gmail.com>
2020-09-24 18:49:31 +0530

    second

--- news_app/models.py
+++ news_app/models.py
@@ -20,8 +20,8 @@ CATEGORY_CHOICES = (
 class Country(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE,default=1 )
     image = models.ImageField(upload_to='pics_country', null=True)
-    heading = models.CharField(max_length=20)
-    title=models.CharField(max_length=40)
+    heading = models.CharField(max_length=100)
+    title=models.CharField(max_length=500)
     content = models.TextField()
     date_time = models.DateTimeField(default=datetime.datetime.now()) 
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
